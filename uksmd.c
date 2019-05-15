b5be94dd428f3f71a0a9d8e833e0cfac25637395
Oleksandr Natalenko <oleksandr@natalenko.name>
2019-05-15 19:53:38 +0200

    scan the list of tasks periodically
    Signed-off-by: Oleksandr Natalenko <oleksandr@natalenko.name>

--- uksmd.c
+++ uksmd.c
@@ -1,26 +1,55 @@
+#include <errno.h>
 #include <proc/readproc.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sysexits.h>
+#include <time.h>
 #include <unistd.h>
+
+#define OBSERVE_WINDOW_SECS	10
+#define IDLE_SLEEP_SECS		5
 
 int main(int _argc, char** _argv)
 {
 	(void)_argc;
 	(void)_argv;
+	pid_t self = getpid();
+	long ctps = sysconf(_SC_CLK_TCK);
 	proc_t proc_info;
+	struct timespec now;
+	unsigned int time_to_sleep;
 
-	memset(&proc_info, 0, sizeof(proc_info));
-
-	PROCTAB* proc = openproc(PROC_FILLSTATUS | PROC_FILLSTAT);
-	while (readproc(proc, &proc_info) != NULL)
+	while (true)
 	{
-		if (!proc_info.vm_size)
+		clock_gettime(CLOCK_BOOTTIME, &now);
+
+		memset(&proc_info, 0, sizeof(proc_info));
+
+		PROCTAB* proc = openproc(PROC_FILLSTATUS | PROC_FILLSTAT);
+		while (readproc(proc, &proc_info) != NULL)
+		{
+			/* skip kthreads */
+			if (!proc_info.vm_size)
+				continue;
+
+			/* skip ourselves */
+			if (proc_info.tid == self)
+				continue;
+
+			/* skip short-living tasks */
+			if (now.tv_sec - proc_info.start_time / ctps < OBSERVE_WINDOW_SECS)
+				continue;
+
+			printf("%u\t%s\n", proc_info.tid, proc_info.cmd);
+		}
+		closeproc(proc);
+
+		time_to_sleep = IDLE_SLEEP_SECS;
+		while ((time_to_sleep = sleep(time_to_sleep)) && errno == EINTR)
 			continue;
-		printf("%u %s %llu\n", proc_info.tid, proc_info.cmd, proc_info.start_time / sysconf(_SC_CLK_TCK));
 	}
-	closeproc(proc);
 
 	exit(EX_OK);
 }
