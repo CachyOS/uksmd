e3968322d73cda243d17f3620d7bd5844bfdcf48
Oleksandr Natalenko <oleksandr@natalenko.name>
2019-05-18 13:09:41 +0200

    this is GPLv3 code
    Signed-off-by: Oleksandr Natalenko <oleksandr@natalenko.name>

--- uksmd.c
+++ uksmd.c
@@ -1,3 +1,20 @@
+/*
+ * uksmd - userspace KSM helper daemon
+ * Copyright (C) 2019 Oleksandr Natalenko <oleksandr@natalenko.name>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <errno.h>
 #include <fcntl.h>
 #include <proc/readproc.h>
